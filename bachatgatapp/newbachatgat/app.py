49db69917b9a28ac015bd634d53a3ff2eb0394f5
pratik3686 <pratik3686@gmail.com>
2024-02-16 16:45:20 +0530

    added readme

--- bachatgatapp/newbachatgat/app.py
+++ bachatgatapp/newbachatgat/app.py
@@ -1,15 +1,15 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import os,Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timedelta
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SECRET_KEY'] = 'bachatgat'
 
 # MySQL Configuration
-app.config['MYSQL_HOST'] = 'database'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'password'
-app.config['MYSQL_DB'] = 'jansevadb'
+app.config['MYSQL_HOST'] = os.getenv('DB_HOST', 'database')
+app.config['MYSQL_USER'] = os.getenv('DB_USER', 'root')
+app.config['MYSQL_PASSWORD'] = os.getenv('DB_PASSWORD', 'password')
+app.config['MYSQL_DB'] = os.getenv('DB_NAME', 'jansevadb')
 
 mysql = MySQL(app)
 
